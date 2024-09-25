09a3a6ad99e8e2726218ffd5a825fc53303b7919
dvirab12 <dvirab12@gmail.com>
2024-09-26 02:40:18 +0300

    need to fix some issues

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 )
 
 var (
-	clients = make(map[net.Conn]string)
+	clients = make(map[string]net.Conn)
 	broadcast chan string
 )
+
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -37,16 +38,18 @@ func handleConnection(conn net.Conn) {
 	fmt.Println("Hello, Please enter your username:")
 	name, _ := reader.ReadString('\n') // listen to input until \n
 	
-	clients[conn] = name
+	clients[name] = conn
 	
-
-	message := []byte("Hello from client")
-	n, err := conn.Write(message)
-	if err != nil {
-		fmt.Println("Couldn't write mesasge to %s: ", conn.RemoteAddr(), err)
-		return
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			leaveMsg, _ := fmt.Printf("%s has left the chat", name)
+			broadcast <- leaveMsg
+			delete(clients, name)
+		}
+		// Broad cast message from user
+		msg, _ :=  fmt.Printf("%s: %s", name, msg)
+		broadcast <- msg
 	}
-	 if n < len(message) {
-		fmt.Println("Message was not fully written %d/%d", n, len(message))
-	 }
+
 }
