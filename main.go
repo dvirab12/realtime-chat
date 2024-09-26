146af9ff473b6209fe61f72e2b4ddf31308fb3ae
dvirab12 <dvirab12@gmail.com>
2024-09-27 00:26:48 +0300

    Finished I'm tired

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
 
+type Client struct {
+	conn net.Conn
+	username string
+	isAdmin bool
+}
+
 var (
-	clients = make(map[string]net.Conn)
-	broadcast chan string
+	clients = []Client{}
+	broadcast = make(chan string)
 )
 
 func main() {
@@ -21,6 +28,8 @@ func main() {
 
 	fmt.Println("Listening on :8080")
 
+	go broadcastMsg()
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -31,25 +40,63 @@ func main() {
 	}
 }
 
+func broadcastMsg() {
+	for {
+		msg := <-broadcast
+
+		for _, client := range clients {
+			_, err := fmt.Fprintf(client.conn, "%s", msg)
+			if err != nil {
+				fmt.Printf("Error sending msg to %s: %v\n", client.username, err)
+				client.conn.Close()
+				removeClient(client.username)
+			}
+		}
+	}
+}
+
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
-	fmt.Println("Hello, Please enter your username:")
-	name, _ := reader.ReadString('\n') // listen to input until \n
-	
-	clients[name] = conn
-	
+	fmt.Fprintf(conn, "Enter your username: ")
+	username, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Printf("Error reading %s username: %v\n", conn.RemoteAddr(), err)
+		return
+	}
+
+	username = strings.TrimSpace(username)
+
+	clients = append(clients, Client{conn, username, false})	
+
+	joinMsg :=fmt.Sprintf("%s has join the chat!\n", username)
+	broadcast <- joinMsg
+
 	for {
 		msg, err := reader.ReadString('\n')
 		if err != nil {
-			leaveMsg, _ := fmt.Printf("%s has left the chat", name)
+			leaveMsg := fmt.Sprintf("%s has left the chat\n", username)
 			broadcast <- leaveMsg
-			delete(clients, name)
+			removeClient(username)
+			return
 		}
-		// Broad cast message from user
-		msg, _ :=  fmt.Printf("%s: %s", name, msg)
-		broadcast <- msg
-	}
 
+		msg = strings.TrimSpace(msg)
+		// Broad cast message from user
+		broadcastMsg :=  fmt.Sprintf("%s: %s", username, msg)
+		broadcast <- broadcastMsg
+	}
+}
+
+// removeClient removes a client from the clients slice
+func removeClient(username string) {
+    for i, client := range clients {
+        if client.username == username {
+            // Remove the client from the slice
+            clients = append(clients[:i], clients[i+1:]...) // Slice removal
+            return
+        }
+    }
 }
